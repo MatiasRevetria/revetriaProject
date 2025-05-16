9ee53cba45202dcc857a377408671fa2d80e6949
mati <revetriamatias03@gmail.com>
2025-05-16 12:39:46 -0300

    Clase 5 v1

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import './styles/product.css'
 
 const Product = ({product, addToCart,removeFromCart})=>{
+
+    const [cantidad, setCantidad] = useState(0)
+
+    const increase = () => setCantidad(prod_selec => (prod_selec < product.stock ? prod_selec + 1 : prod_selec));
+
+    const decrease = () => setCantidad(prod_delete => (prod_delete > 0 ? prod_delete - 1 : 0));
+
     return(
         <div>
-            <span>{product.nombre} - {product.precio}</span>
-            <button onClick={() => addToCart(product)}>Agregar</button>
-            <button onClick={() => removeFromCart(product)}>Eliminar</button>
+            <div id="product_image">
+                <img src={product.imagen} alt="imagen" />
+            </div>
+            <div>{product.nombre} </div>
+            <div id="product_price">{product.precio}</div>
+            {/* <button onClick={() => addToCart(product)}>+</button> */}
+            <button onClick={()=>{increase()}}>+</button>
+            <span>{cantidad}</span>
+            <button onClick={()=>{decrease()}}>-</button>
+            {/* <button onClick={() => removeFromCart(product)}>-</button> */}
         </div>
     )
 
--- src/layout/home.jsx
+++ src/layout/home.jsx
@@ -9,17 +9,21 @@ import Formulario from "../components/Formulario"
 import { prodructList } from "../utils/data";
 import ProductList from "../components/ProductList";
 import Cart from "../components/Cart";
-
+import loading from "../assets/Loading_icon.gif"
 
 const Home = ({cart,handlerAddToCart,handlerRemoveFromCart}) => {
     const countItem = cart.length;
     const [productos, setProductos] = useState([]);
+    const [carga, setCarga] = useState(true);
 
     useEffect(()=>{
       fetch('https://681d9331f74de1d219b03777.mockapi.io/productos')
         .then((respuesta) => respuesta.json())
-        .then((datos) => setProductos(datos))
-        .catch((error) => console.error('Error:', error));
+        .then((datos) => {setProductos(datos) 
+                          setCarga(false)})
+        .catch((error) => {console.error('Error:', error)
+                           setCarga(false)
+        });
 
     },[]);
 
@@ -28,7 +32,10 @@ const Home = ({cart,handlerAddToCart,handlerRemoveFromCart}) => {
         <Header />
         <Nav countItem={countItem}/>
         <Main />
-        <ProductList products={productos} addToCart={handlerAddToCart} removeFromCart={handlerRemoveFromCart}/>
+        {
+          carga ? <img src={loading} alt="loading" /> :  <ProductList products={productos} addToCart={handlerAddToCart} removeFromCart={handlerRemoveFromCart}/>
+        }
+      
         <Cart cartItems={cart}/>
         <Gallery />
         <Formulario />
